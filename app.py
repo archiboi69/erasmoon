6d20a24d630bdfa14aac6bb2e11d6579ab6e166c
archiboi69 <157481951+archiboi69@users.noreply.github.com>
2024-09-29 13:24:28 +0200

    changed default debug to False

--- app.py
+++ app.py
@@ -122,5 +122,5 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    debug = os.environ.get('FLASK_DEBUG', True)
+    debug = os.environ.get('FLASK_DEBUG', False)
     app.run(debug=debug, host='0.0.0.0', port=port)
