b8e2f704ea9c9cad33f54d427f6c4e870158559c
archiboi69 <157481951+archiboi69@users.noreply.github.com>
2024-09-29 12:03:23 +0200

    reverted database.py for sqlite

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from models import Base
 
 DATABASE_URL = 'sqlite:///instance/cities.db'
 
-engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
-db_session = scoped_session(sessionmaker(bind=engine))
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
     Base.metadata.create_all(bind=engine)
